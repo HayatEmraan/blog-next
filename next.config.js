4f0e304b6335397e88f833d16c62efa61b628b69
hayatemraan <emraanhasanhayat@gmail.com>
2023-08-23 22:23:04 +0600

    'updated'

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: "https://basic-blog.teamrabbil.com/api",
   },
+  distDir: "build",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
